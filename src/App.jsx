fd1aceec337dd63993b3553ca10c2b7d1906be44
FullyAngozi <ochuks33@yahoo.com>
2023-02-06 01:02:22 -0500

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className=" bg-primary w-full overflow-hidden ">
+    <div className=" bg-primary w-full ">
       <div className=' sm:px-16 px-6 flex justify-center items-center'>
         <div className=' xl:max-w-[1280px] w-full'>
           <Navbar />
         </div>
       </div>
 
-      <div className=' bg-primary flex justify-center items-start'>
+      <div className=' bg-primary flex justify-center items-center'>
         <div className=' max-w-[1280px]'>
           <Hero />
         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,23 +3,23 @@ import { heroimage, heroimageBig, clients } from "../constants";
 
 const Hero = () => {
   return (
-    <main className=" ">
-      <div>
-        <img src={heroimage} alt="talents" className="my-5 md:hidden" />
-        <img src={heroimageBig} alt="talents" className="my-5 hidden md:flex" />
+    <main className=" bg-primary sm:flex flex-row-reverse mx-auto">
+      <div className=" flex  justify-center items-center md:my-0 my-10 p-6 md:p-0">
+        <img src={heroimage} alt="talents" className=" w-[100%] h-[100%] md:hidden " />
+        <img src={heroimageBig} alt="talents" className="hidden md:flex w-[400px]" />
       </div>
 
-      <div className=" bg-purple-300 flex flex-col justify-center"> 
-        <div className=" ">
-          <h1>Make remote work</h1>
-          <p>Get your team in sync, no matter your location. streamline processes, create team rituals, and watch productivity soar.</p>
-          <span>
+      <div className="  flex flex-col "> 
+        <div className=" flex flex-col items-center text-center md:text-left md:items-start">
+          <h1 className="font-bold ss:text-[68px] text-[52px] leading-tight">Make <br className="hidden md:flex"/> remote work</h1>
+          <p className=" w-[400px] my-3 text-2xl text-Secondary leading-[30px] ">Get your team in sync, no matter your location. streamline processes, create team rituals, and watch productivity soar.</p>
+          <span className=" py-3 px-6 border border-Secondary rounded-2xl bg-dark text-primary my-4">
             <a href="">Learn more</a>
           </span>
         </div>
-        <div className=" flex">
+        <div className=" flex items-center justify-around mb-20 w-[480px] mx-auto mt-auto md:mb-0 ">
           {clients.map(item => (
-            <img src={item.logo} alt='logos' key={item.id} />
+            <img src={item.logo} alt='logos' key={item.id} className=" mr-3" />
           ))}
         </div>
       </div>
